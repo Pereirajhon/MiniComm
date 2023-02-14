161c99280423ee29f38216e808e1fc03668985e5
Joao Vitor <jvpereira1010@gmail.com>
2023-02-14 20:27:59 -0300

    Implementação do header

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,16 +1,35 @@
 import {HeaderContainer, HeaderContent} from './styles'
+import { NavLink } from 'react-router-dom'
 import {useAppSelector} from '../../store/store'
 
 
 export const Header = () => {
-    const {product} = useAppSelector(state => state.product)
+    const {cart} = useAppSelector(state => state.cart)
+
+    //console.log(quantityItem)
+   // const items: Items[] = JSON.parse(localStorage.getItem('cartItems'))
+    //console.log(items)
+    const quant = cart?.map(item => item.quantity).reduce((acc, q) => acc += q, 0)
+       
+    //console.log(quant)
 
     return(
         <HeaderContainer>
             <HeaderContent>
-                <h2>MiniComm
-                </h2>
-                <span>{product}</span>
+                <NavLink to='/' >
+                    <h2>MiniComm</h2> 
+                </NavLink>
+                <NavLink to='/cart' >
+                    <div className="material-symbols-outlined">
+                        shopping_cart
+                        { quant > 0 ? (
+                            <span> {quant} </span>
+                            ) : ''}
+                    </div>
+                </NavLink>
+                
+                
+ 
             </HeaderContent>
         </HeaderContainer>
     )
--- src/component/Header/styles.ts
+++ src/component/Header/styles.ts
@@ -4,14 +4,33 @@ export const HeaderContainer = styled.header`
     max-width: 100%;
     height: 2rem;
     padding: 2rem;
-    background-color: #0060b1;
+    background-color: ${props => props.theme.blue};
     color: #fff;
 `
 
 export const HeaderContent = styled.div`
     width: 100%;
     height: 100%;
+    padding: 0 3rem;
     display: flex;
-    align-items:center;
+    align-items: center;
     justify-content: space-between;
+
+    .material-symbols-outlined{
+        display: flex ;
+        span {
+            display: flex;
+            margin-top: -25% ;
+            width: 100%;
+            height: 100%;
+            font-size: 0.8rem;
+            padding: 0.3rem;
+            color: #fff ;
+            font-weight: bold ;
+            background-color: rgb(227, 82, 20);
+            border-radius: 50px;
+
+        }
+
+    }
 `
